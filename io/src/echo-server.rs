9044bff74be9a5680a66c86d5b7d2d9bcd1b070e
gui <guisarria@gmail.com>
2024-07-27 22:53:08 -0300

    init

---
+++ io/src/echo-server.rs
@@ -0,0 +1,13 @@
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    let mut buffer = [0; 10];
+
+    let n = f.read(&mut buffer[..]).await?;
+
+    println!("The bytes: {:?}", &buffer[..n]);
+    Ok(())
+}
